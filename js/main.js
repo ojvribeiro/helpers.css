82f3ea31f4e215021ce94c7e2b222c7ecadcde81
Victor Ribeiro <devjvribeiro@gmail.com>
2020-07-18 06:54:40 -0300

    Clone nav menu into side menu

--- js/main.js
+++ js/main.js
@@ -1,23 +1,36 @@
 let prevHash = undefined, scrollToTop;
 
-$('.collapsible').collapsible();
-$('.tooltipped').tooltip();
 
-$('.scrollspy').scrollSpy({
-  scrollOffset: 20
-});
 
-routie({
-  '/:page': (page) => {
-    loadPage(page);
-    
-    goToTop();
-    // console.log('elements');
+
+function init () {
+  const navMenu = document.getElementById('slide-out');
+  const sideMenu = document.getElementById('side-menu');
+  updateInnerHTML(sideMenu, 'update', navMenu.innerHTML);
+  
+
+  $('.collapsible').collapsible();
+  $('.tooltipped').tooltip();
+  
+  $('.scrollspy').scrollSpy({
+    scrollOffset: 20
+  });
+  
+  routie({
+    '/:page': (page) => {
+      loadPage(page);
+      
+      goToTop();
+      // console.log('elements');
+    }
+  });
+  
+  if (location.hash === '') {
+    routie('/get-started/');
   }
-});
-
-if (location.hash === '') {
-  routie('/get-started/');
+  
+  // console.log('DOMContentLoaded');
+  
 }
 
 
@@ -25,7 +38,7 @@ if (location.hash === '') {
 function loadPage (file) {
   const path = './pages/';
   const extension = '.html';
-
+  
   progressBar().show();
   
   $.ajax({
@@ -40,33 +53,33 @@ function loadPage (file) {
         // console.log(prevHash, currHash, file);
         if (currHash !== prevHash || prevHash === undefined) {
           const collectionItem = document.querySelectorAll('.collection-item');
-
+          
           updateInnerHTML('main', 'update', data);
           Prism.highlightAll();
           add(pageContent, 'showPage');
-
+          
           progressBar().size(100);
           progressBar().hide();
-
-
+          
+          
           collectionItem.forEach(function (el) {
             el.classList.remove('active');
           });
-    
+          
           setTimeout(function() {
-
+            
             if (file) {
               let item = document.querySelectorAll('.sidebar ul li');
               
               for (let i in item) {
                 // console.log(file, item[i].id, item);
                 if (item[i].id === file) {
-                  $('.collapsible').collapsible('open', i);
+                  $('.collapsible').collapsible('open', i - 1);
                   break;
                 }
               }
             }
-
+            
             $('.scrollspy').scrollSpy({
               scrollOffset: 100
             });
@@ -101,33 +114,33 @@ function updateInnerHTML (element, insertType = undefined, content) {
             el.classList.remove('showItem');
           });
         });
-
-      break;
-      case 'prepend':
-        elementOnDOM.prepend(content)
-        .find('.showItem')
-        .one('animationend',function () {
-          showItem.forEach(function (el) {
-            el.classList.remove('showItem');
-          });
-        });
+        
         break;
-        default: // update
-        elementOnDOM.html(content);
+        case 'prepend':
+          elementOnDOM.prepend(content)
+          .find('.showItem')
+          .one('animationend',function () {
+            showItem.forEach(function (el) {
+              el.classList.remove('showItem');
+            });
+          });
+          break;
+          default: // update
+          elementOnDOM.html(content);
+        }
+      }
     }
-  }
-}
-  
-  
-  
-  
-function add (element, animationName, callback) {
-  if (element) {
-    element.classList.add('animated', 'faster', animationName);
-
-    element.addEventListener('animationend', function () {
-      if (!animationName.match(/out/gi)) {
-        element.classList.remove('faster', animationName);
+    
+    
+    
+    
+    function add (element, animationName, callback) {
+      if (element) {
+        element.classList.add('animated', 'faster', animationName);
+        
+        element.addEventListener('animationend', function () {
+          if (!animationName.match(/out/gi)) {
+            element.classList.remove('faster', animationName);
       }
       
       if (callback !== undefined) callback();
@@ -139,13 +152,13 @@ function add (element, animationName, callback) {
 function goToTop() {
   scrollToTop = () => {
     const c = document.documentElement.scrollTop || document.body.scrollTop;
-
+    
     if (c > 0) {
       window.requestAnimationFrame(scrollToTop);
       window.scrollTo(0, c - c / 8);
     }
   };
-
+  
   scrollToTop();
 }
 
@@ -160,26 +173,26 @@ function progressBar() {
         let theBar = document.getElementsByClassName('helpers-progress')[0];
         if (progressBar) {
           theBar.classList.add('show');
-  
+          
           clearInterval(loop);
         }
       }, 300);
     },
-  
+    
     hide: () => {
       setTimeout(function () {
         let theBar = document.getElementsByClassName('helpers-progress')[0];
         theBar.classList.remove('show');
-  
+        
         setTimeout(function () {
           progressBar().size(15);
         }, 1000);
       }, 500);
     },
-  
+    
     size: (percentage) => {
       let theBar = document.querySelector('.helpers-progress .helpers-progress--bar');
-  
+      
       theBar.style.width = percentage + '%';
     }
   }
@@ -192,4 +205,8 @@ $('.sidenav').sidenav({
 
 $('.sidenav .collection-item').on('click', function () {
   $('.sidenav').sidenav('close');
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  init();
 });
